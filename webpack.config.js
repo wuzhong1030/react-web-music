1943290170a65aa58803d65040b862b703496786
吴忠 <wuzhong1598@qq.com>
2018-07-07 13:02:17 +0800

    commit demo code

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,13 @@
+var path = require('path')
+function resolve(dir) {
+    return path.join(__dirname, './', dir)
+  }
 module.exports = {
+    context: path.resolve(__dirname, './'),
+    mode: 'development',
     entry: './app/index.js',
     output: {
-        path: './dist',
+        path: resolve('dist'),
         filename: 'bundle.js'
     }
 }
