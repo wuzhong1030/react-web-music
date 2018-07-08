d9fc8521686b68fb3a153491ed2e1bba87681513
吴忠 <wuzhong1598@qq.com>
2018-07-08 17:41:48 +0800

    commit code

---
+++ app/pages/Player/index.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+
+class Player extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            progress: 0,
+            volume: 0,
+            isPlay: true,
+            leftTime: ''
+        }
+    }
+    componentDidMount() {
+		$("#player").bind($.jPlayer.event.timeupdate, (e) => {
+			duration = e.jPlayer.status.duration;
+			this.setState({
+				progress: e.jPlayer.status.currentPercentAbsolute,
+				volume: e.jPlayer.options.volume * 100,
+				leftTime: this.formatTime(duration * (1 - e.jPlayer.status.currentPercentAbsolute / 100))
+			});
+		});
+	}
+	componentWillUnmount() {
+		$("#player").unbind($.jPlayer.event.timeupdate);
+	}
+    render() {
+
+    }
+}
